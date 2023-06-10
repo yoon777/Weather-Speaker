c3e0155a583a8e2f278d42192a89f0a2e57341a9
sangjin <koggk1@gmail.com>
2023-06-10 20:20:53 +0900

    android alarm
    안드로이드 알람 코드입니다. 좀 더 수정하고 올리려고 했으나 시간이 걸릴것 같아서 먼저 업로드 합니다.

---
+++ Alarm/app/src/main/java/com/example/myapplication/AlarmReceiver.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent sIntent = new Intent(context, AlarmService.class);
+        sIntent.putExtra("state", intent.getStringExtra("state"));
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            context.startForegroundService(sIntent);
+        } else {
+            context.startService(sIntent);
+        }
+    }
+}
---
+++ Alarm/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.myapplication;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AlarmManager alarmManager;
+    private TimePicker timePicker;
+
+    private PendingIntent pendingIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        this.timePicker = findViewById(R.id.timePicker);
+
+        findViewById(R.id.btnStart).setOnClickListener(mClickListener);
+        findViewById(R.id.btnStop).setOnClickListener(mClickListener);
+    }
+
+    // 알람 시작
+    private void start() {
+        // 시간 설정
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, this.timePicker.getHour());
+        calendar.set(Calendar.MINUTE, this.timePicker.getMinute());
+        calendar.set(Calendar.SECOND, 0);
+
+        // 현재시간보다 이전이면
+        if (calendar.before(Calendar.getInstance())) {
+            // 다음날로 설정
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        // Receiver 설정
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        // state 값이 on 이면 알람시작, off 이면 중지
+        intent.putExtra("state", "on");
+
+        this.pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        // 알람 설정
+        this.alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+        // Toast 보여주기 (알람 시간 표시)
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Toast.makeText(this, "Alarm : " + format.format(calendar.getTime()), Toast.LENGTH_SHORT).show();
+    }
+
+    // 알람 중지
+    private void stop() {
+        if (this.pendingIntent == null) {
+            return;
+        }
+
+        // 알람 취소
+        this.alarmManager.cancel(this.pendingIntent);
+
+        // 알람 중지 Broadcast
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        intent.putExtra("state","off");
+
+        sendBroadcast(intent);
+
+        this.pendingIntent = null;
+    }
+
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            if (id == R.id.btnStart) {
+                // 알람 시작
+                start();
+            }
+
+            else if (id == R.id.btnStop) {
+                // 알람 중지
+                stop();
+            }
+        }
+    };
+}
