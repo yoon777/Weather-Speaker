7a291a5a7ba1cbb8db95b3fc3c2fc3272b059f7e
poolmu <ektlxl98@gmail.com>
2023-06-21 17:48:17 +0900

    최종본

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,428 @@
+package com.example.myapplication;
+
+import android.Manifest;
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.PendingIntent;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+import android.util.Log;
+import android.view.View;
+
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
+
+
+
+
+public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener {
+
+    private AlarmManager alarmManager;
+    private TimePicker timePicker;
+
+    private PendingIntent pendingIntent;
+
+    private TextToSpeech tts;
+
+    private GpsTracker gpsTracker;
+    private TransLocalPoint transLocalPoint;
+
+    public String jsresult;
+    public String weather_result;
+    public String date = "";
+
+
+    int api_x;
+    int api_y;
+
+    Weather_json wjson = new Weather_json();
+
+    private static final int GPS_ENABLE_REQUEST_CODE = 2001;
+    private static final int PERMISSIONS_REQUEST_CODE = 100;
+    String[] REQUIRED_PERMISSIONS  = {android.Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        this.timePicker = findViewById(R.id.timePicker);
+
+        findViewById(R.id.btnStart).setOnClickListener(mClickListener);
+        findViewById(R.id.btnStop).setOnClickListener(mClickListener);
+
+        tts = new TextToSpeech(this, this);
+
+        ApiThread apiThread = new ApiThread();
+        PThread pThread = new PThread();
+
+        gpsTracker = new GpsTracker(MainActivity.this);
+
+        double latitude = gpsTracker.getLatitude();
+        double longitude = gpsTracker.getLongitude();
+
+        Log.e("latlong", "lat = " + latitude + " long = " + longitude);
+
+        //기상청 격자 좌표 변환
+        transLocalPoint = new TransLocalPoint();
+        TransLocalPoint.LatXLngY tmp = transLocalPoint.convertGRID_GPS(0, latitude, longitude);
+        api_x = (int) tmp.x;
+        api_y = (int) tmp.y;
+        Log.e(">>", "x = " + tmp.x + ", y = " + tmp.y);
+        //격자구하기 끝
+
+        long mNow = System.currentTimeMillis();
+        Date mReDate = new Date(mNow);
+        SimpleDateFormat mFormatYDM = new SimpleDateFormat("yyyyMMdd"); //ex)20230210
+        date = mFormatYDM.format(mReDate);
+        System.out.println(date);
+
+        Log.d("date", "현재 날짜? : " + date);
+
+
+        try{
+            apiThread.start();
+            apiThread.join();
+
+            pThread.start();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if (!checkLocationServicesStatus()) {
+
+            showDialogForLocationServiceSetting();
+        }else {
+
+            checkRunTimePermission();
+        }
+
+
+
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int permsRequestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grandResults) {
+
+        super.onRequestPermissionsResult(permsRequestCode, permissions, grandResults);
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+
+            // 요청 코드가 PERMISSIONS_REQUEST_CODE 이고, 요청한 퍼미션 개수만큼 수신되었다면
+
+            boolean check_result = true;
+
+
+            // 모든 퍼미션을 허용했는지 체크합니다.
+
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+
+            if (check_result) {
+
+                //위치 값을 가져올 수 있음
+                ;
+            } else {
+                // 거부한 퍼미션이 있다면 앱을 사용할 수 없는 이유를 설명해주고 앱을 종료합니다.2 가지 경우가 있습니다.
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.", Toast.LENGTH_LONG).show();
+                    finish();
+
+
+                } else {
+
+                    Toast.makeText(MainActivity.this, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", Toast.LENGTH_LONG).show();
+
+                }
+            }
+
+        }
+    }
+
+    void checkRunTimePermission(){
+
+        //런타임 퍼미션 처리
+        // 1. 위치 퍼미션을 가지고 있는지 체크합니다.
+        int hasFineLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+
+        if (hasFineLocationPermission == PackageManager.PERMISSION_GRANTED &&
+                hasCoarseLocationPermission == PackageManager.PERMISSION_GRANTED) {
+
+            // 2. 이미 퍼미션을 가지고 있다면
+            // ( 안드로이드 6.0 이하 버전은 런타임 퍼미션이 필요없기 때문에 이미 허용된 걸로 인식합니다.)
+
+
+            // 3.  위치 값을 가져올 수 있음
+
+
+
+        } else {  //2. 퍼미션 요청을 허용한 적이 없다면 퍼미션 요청이 필요합니다. 2가지 경우(3-1, 4-1)가 있습니다.
+
+            // 3-1. 사용자가 퍼미션 거부를 한 적이 있는 경우에는
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, REQUIRED_PERMISSIONS[0])) {
+
+                // 3-2. 요청을 진행하기 전에 사용자가에게 퍼미션이 필요한 이유를 설명해줄 필요가 있습니다.
+                Toast.makeText(MainActivity.this, "이 앱을 실행하려면 위치 접근 권한이 필요합니다.", Toast.LENGTH_LONG).show();
+                // 3-3. 사용자게에 퍼미션 요청을 합니다. 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+
+
+            } else {
+                // 4-1. 사용자가 퍼미션 거부를 한 적이 없는 경우에는 퍼미션 요청을 바로 합니다.
+                // 요청 결과는 onRequestPermissionResult에서 수신됩니다.
+                ActivityCompat.requestPermissions(MainActivity.this, REQUIRED_PERMISSIONS,
+                        PERMISSIONS_REQUEST_CODE);
+            }
+
+        }
+
+    }
+
+    // 알람 시작
+    private void start() {
+        // 시간 설정
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.HOUR_OF_DAY, this.timePicker.getHour());
+        calendar.set(Calendar.MINUTE, this.timePicker.getMinute());
+        calendar.set(Calendar.SECOND, 0);
+
+        // 현재시간보다 이전이면
+        if (calendar.before(Calendar.getInstance())) {
+            // 다음날로 설정
+            calendar.add(Calendar.DATE, 1);
+        }
+
+        // Receiver 설정
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        // state 값이 on 이면 알람시작, off 이면 중지
+        intent.putExtra("state", "on");
+
+        this.pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT| PendingIntent.FLAG_IMMUTABLE);
+
+        // 알람 설정
+        this.alarmManager.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), pendingIntent);
+
+        // Toast 보여주기 (알람 시간 표시)
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+        Toast.makeText(this, "Alarm : " + format.format(calendar.getTime()), Toast.LENGTH_SHORT).show();
+    }
+
+    // 알람 중지
+    private void stop() {
+        if (this.pendingIntent == null) {
+            return;
+
+
+        }
+
+        // 알람 취소
+        this.alarmManager.cancel(this.pendingIntent);
+
+        // 알람 중지 Broadcast
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        intent.putExtra("state","off");
+
+        sendBroadcast(intent);
+
+        this.pendingIntent = null;
+        speakOut();
+        Toast.makeText(getApplicationContext(), weather_result,Toast.LENGTH_LONG).show();
+
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void speakOut(){
+        CharSequence text = weather_result;
+        tts.setPitch((float)1.0); // 음성 톤 높이 지정
+        tts.setSpeechRate((float)1.0); // 음성 속도 지정
+
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "id1");
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.i("tts","onDestroy 부름");
+        if(tts!=null){ // 사용한 TTS객체 제거
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onDestroy();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onInit(int status) { // OnInitListener를 통해서 TTS 초기화
+        Log.i("tts","onInit 부름");
+        if(status == TextToSpeech.SUCCESS){
+            int result = tts.setLanguage(Locale.KOREA); // TTS언어 한국어로 설정
+
+            if(result == TextToSpeech.LANG_NOT_SUPPORTED || result == TextToSpeech.LANG_MISSING_DATA){
+                Log.e("TTS", "This Language is not supported");
+            }else{
+
+            }
+        }else{
+            Log.e("TTS", "Initialization Failed!");
+        }
+    }
+    View.OnClickListener mClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int id = v.getId();
+            if (id == R.id.btnStart) {
+                // 알람 시작
+                start();
+            }
+
+            else if (id == R.id.btnStop) {
+                // 알람 중지
+                stop();
+
+
+            }
+        }
+    };
+
+    private void showDialogForLocationServiceSetting() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setTitle("위치 서비스 비활성화");
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.\n"
+                + "위치 설정을 수정하실래요?");
+        builder.setCancelable(true);
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                Intent callGPSSettingIntent
+                        = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.cancel();
+            }
+        });
+        builder.create().show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+
+            case GPS_ENABLE_REQUEST_CODE:
+
+                //사용자가 GPS 활성 시켰는지 검사
+                if (checkLocationServicesStatus()) {
+                    if (checkLocationServicesStatus()) {
+
+                        Log.d("@@@", "onActivityResult : GPS 활성화 되있음");
+                        checkRunTimePermission();
+                        return;
+                    }
+                }
+
+                break;
+        }
+    }
+
+    public boolean checkLocationServicesStatus() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+
+
+    private class ApiThread extends Thread{
+
+        public ApiThread(){
+        }
+
+        @Override
+        public void run() {
+            try {
+
+                apiTest api_result = new apiTest();
+
+
+                jsresult = api_result.func(date, api_x, api_y);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+    private class PThread extends Thread {
+
+        public PThread(){
+
+        }
+
+        @Override
+        public void run() {
+            if (jsresult != null) {
+                try {
+
+                    weather_result = wjson.search(jsresult);
+                }
+
+                catch (JSONException e) {
+                    throw new RuntimeException(e);
+                }
+
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/Weather_json.java
@@ -0,0 +1,97 @@
+package com.example.myapplication;
+
+
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
+public class Weather_json {
+    private String tmx = "", tmn = "", time = "", tmp_hour = "";
+    private
+
+
+    ArrayList<String> time_array = new ArrayList<String>();
+
+
+    public String search(String result) throws JSONException {
+
+        try {
+
+
+            // response 키를 가지고 데이터를 파싱
+            JSONObject jsonObj_1 = new JSONObject(result);
+            String response = jsonObj_1.getString("response");
+
+            // response 로 부터 body 찾기
+            JSONObject jsonObj_2 = new JSONObject(response);
+            String body = jsonObj_2.getString("body");
+
+            // body 로 부터 items 찾기
+            JSONObject jsonObj_3 = new JSONObject(body);
+            String items = jsonObj_3.getString("items");
+            Log.i("ITEMS", items);
+
+            // items로 부터 itemlist 를 받기
+            JSONObject jsonObj_4 = new JSONObject(items);
+            JSONArray jsonArray = jsonObj_4.getJSONArray("item");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonObj_4 = jsonArray.getJSONObject(i);
+                String fcstTime = jsonObj_4.getString("fcstTime");
+                String fcstValue = jsonObj_4.getString("fcstValue");
+                String category = jsonObj_4.getString("category");
+
+                if (category.equals("TMX")) {
+
+
+                    tmx = "오늘 최고 기온은 " + fcstValue + "℃" + "이고 ";
+
+                }
+
+                if (category.equals("TMN")) {
+                    tmn = "최저 기온은 " + fcstValue + "℃" + "입니다. ";
+                }
+
+                if (category.equals("PTY")) {
+                    if(!fcstValue.equals("0")){
+                        time_array.add(fcstTime.substring(0,2));
+                    }
+
+
+                }
+
+            }
+
+
+            if(time_array.size() != 0){
+
+               
+                time = String.join("시, ", time_array) + "시에 강수가 있습니다";
+            }
+            else{
+                time = "";
+            }
+
+
+        }
+        catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+
+
+        return tmx + tmn + time;
+    }
+
+
+
+
+
+}
